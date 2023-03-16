61426790f0d73658215af4cb0a7ee92d363a99c7
mahesh <mahesh.k.ca.us@gmail.com>
2023-03-16 06:59:02 +0530

    Update computational models in test_signals.py

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -4,3 +4,9 @@ import numpy as np
 
 from unsteady_flow.analysis import estimate_shock_position
 from unsteady_flow.signals import Signal, damped_tone, shock_interaction
+
+
+class SignalTests(unittest.TestCase):
+    def test_rejects_frequency_above_nyquist(self):
+        with self.assertRaisesRegex(ValueError, "Nyquist"):
+            damped_tone(sample_rate=1000, frequency=600)
