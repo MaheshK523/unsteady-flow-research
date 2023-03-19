58f3749635484ec7f28f46ac17fca4e18d1cc07f
mahesh <mahesh.k.ca.us@gmail.com>
2023-03-19 11:16:46 +0530

    Update computational models in test_signals.py

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -10,3 +10,9 @@ class SignalTests(unittest.TestCase):
     def test_rejects_frequency_above_nyquist(self):
         with self.assertRaisesRegex(ValueError, "Nyquist"):
             damped_tone(sample_rate=1000, frequency=600)
+
+    def test_shock_location_is_recovered(self):
+        signal = shock_interaction(points=2048, shock_position=0.37, thickness=0.005)
+        estimate = estimate_shock_position(signal)
+        self.assertAlmostEqual(estimate["shock_position"], 0.37, delta=0.004)
+
