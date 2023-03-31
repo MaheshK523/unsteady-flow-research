43c36265b843caf272394a9630c867b3cc3068dc
mahesh <mahesh.k.ca.us@gmail.com>
2023-04-01 03:42:02 +0530

    Update computational models in test_spectrum.py

--- tests/test_spectrum.py
+++ tests/test_spectrum.py
@@ -4,3 +4,9 @@ from unsteady_flow.signals import damped_tone, wind_turbine_signal
 from unsteady_flow.spectrum import one_sided_fft, welch_psd
 
 
+class SpectrumTests(unittest.TestCase):
+    def test_fft_recovers_known_tone(self):
+        signal = damped_tone(duration=0.1, sample_rate=20_000, frequency=1_250, damping_rate=10)
+        dominant = one_sided_fft(signal).dominant_frequency(1)
+        self.assertAlmostEqual(dominant, 1_250, delta=12)
+
