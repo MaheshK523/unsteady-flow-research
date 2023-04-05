c097e012aeb6b5e6e40b1014dad5efcb6f2eb61a
mahesh <mahesh.k.ca.us@gmail.com>
2023-04-05 09:10:44 +0530

    Update computational models in test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,3 +4,9 @@ from pathlib import Path
 
 from unsteady_flow.cli import run_scenario
 
+
+class CliTests(unittest.TestCase):
+    def test_jet_scenario_writes_reproducible_artifacts(self):
+        with tempfile.TemporaryDirectory() as directory:
+            summary = run_scenario("jet", directory, {"frequency": 2000, "sample_rate": 40000, "duration": 0.02})
+            root = Path(directory) / "jet"
