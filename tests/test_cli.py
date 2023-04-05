6753fcc826d78771ab351c6aecaaebf6d572142f
mahesh <mahesh.k.ca.us@gmail.com>
2023-04-05 11:17:47 +0530

    Update computational models in test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,3 +10,9 @@ class CliTests(unittest.TestCase):
         with tempfile.TemporaryDirectory() as directory:
             summary = run_scenario("jet", directory, {"frequency": 2000, "sample_rate": 40000, "duration": 0.02})
             root = Path(directory) / "jet"
+            self.assertAlmostEqual(summary["dominant_fft_hz"], 2000, delta=60)
+            for name in ["signal.csv", "fft.csv", "welch.csv", "summary.json", "analysis.png"]:
+                self.assertTrue((root / name).is_file(), name)
+
+
+if __name__ == "__main__":
