fece3b30aa6e87a10376f7880bd47c501296d50a
mahesh <mahesh.k.ca.us@gmail.com>
2023-05-15 11:03:12 +0530

    Update computational models in signals.py

--- unsteady_flow/signals.py
+++ unsteady_flow/signals.py
@@ -10,3 +10,9 @@ class Signal:
     coordinate: np.ndarray
     values: np.ndarray
     coordinate_name: str
+    coordinate_unit: str
+    value_name: str
+    value_unit: str
+
+    def __post_init__(self) -> None:
+        if self.coordinate.ndim != 1 or self.values.ndim != 1:
