1ec0054d750b820143c48b0e1524b5de9d0b106b
mahesh <mahesh.k.ca.us@gmail.com>
2023-06-25 03:53:21 +0530

    Update computational models in analysis.py

--- unsteady_flow/analysis.py
+++ unsteady_flow/analysis.py
@@ -4,3 +4,9 @@ import numpy as np
 
 from .signals import Signal
 from .spectrum import one_sided_fft, welch_psd
+
+
+def summarize_temporal_signal(signal: Signal, welch_segment: int | None = None) -> dict[str, float | int | str]:
+    segment = welch_segment or min(256, len(signal.values))
+    spectrum = one_sided_fft(signal)
+    psd = welch_psd(signal, segment_length=segment)
