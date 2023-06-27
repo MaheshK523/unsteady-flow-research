72a0bacfe34927d58ea4c0348027871bbf2662b0
mahesh <mahesh.k.ca.us@gmail.com>
2023-06-27 13:40:38 +0530

    Update computational models in analysis.py

--- unsteady_flow/analysis.py
+++ unsteady_flow/analysis.py
@@ -10,3 +10,9 @@ def summarize_temporal_signal(signal: Signal, welch_segment: int | None = None) 
     segment = welch_segment or min(256, len(signal.values))
     spectrum = one_sided_fft(signal)
     psd = welch_psd(signal, segment_length=segment)
+    return {
+        "samples": len(signal.values),
+        "duration": float(signal.coordinate[-1] - signal.coordinate[0]),
+        "sample_rate_hz": 1.0 / signal.sample_spacing,
+        "mean": float(np.mean(signal.values)),
+        "rms": float(np.sqrt(np.mean(signal.values**2))),
