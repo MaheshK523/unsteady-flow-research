fd002fb744713f483c975106dbc72ff40c5c9a13
mahesh <mahesh.k.ca.us@gmail.com>
2023-07-12 21:23:20 +0530

    Update computational models in io.py

--- unsteady_flow/io.py
+++ unsteady_flow/io.py
@@ -10,3 +10,9 @@ from .spectrum import Spectrum
 
 def write_signal_csv(signal: Signal, path: str | Path) -> Path:
     target = Path(path)
+    target.parent.mkdir(parents=True, exist_ok=True)
+    with target.open("w", newline="", encoding="utf-8") as handle:
+        writer = csv.writer(handle)
+        writer.writerow([f"{signal.coordinate_name}_{signal.coordinate_unit}", f"{signal.value_name}_{signal.value_unit}"])
+        writer.writerows(zip(signal.coordinate, signal.values, strict=True))
+    return target
