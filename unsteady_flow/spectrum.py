5a8bd447ccb2e388009ca56017b518747a848462
mahesh <mahesh.k.ca.us@gmail.com>
2023-09-09 01:08:05 +0530

    Update computational models in spectrum.py

--- unsteady_flow/spectrum.py
+++ unsteady_flow/spectrum.py
@@ -10,3 +10,9 @@ from .signals import Signal
 @dataclass(frozen=True)
 class Spectrum:
     frequency: np.ndarray
+    magnitude: np.ndarray
+    kind: str
+    unit: str
+
+    def dominant_frequency(self, minimum_frequency: float = 0.0) -> float:
+        mask = self.frequency >= minimum_frequency
