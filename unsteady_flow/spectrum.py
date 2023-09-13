aaa43dd39651370a5bd5d8109c864ccddec9f31e
mahesh <mahesh.k.ca.us@gmail.com>
2023-09-14 03:40:37 +0530

    Update computational models in spectrum.py

--- unsteady_flow/spectrum.py
+++ unsteady_flow/spectrum.py
@@ -16,3 +16,9 @@ class Spectrum:
 
     def dominant_frequency(self, minimum_frequency: float = 0.0) -> float:
         mask = self.frequency >= minimum_frequency
+        if not np.any(mask):
+            raise ValueError("minimum_frequency is outside the spectrum")
+        frequency = self.frequency[mask]
+        magnitude = self.magnitude[mask]
+        return float(frequency[int(np.argmax(magnitude))])
+
